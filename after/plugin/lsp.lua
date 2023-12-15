aff1b06a9f1f3efc9df306d36e2051ae10879f28
ZioTino <m.chiodaiolo@gmail.com>
2023-12-15 07:28:16 +0100

    Fixed auto-loading of lsp and treesitter config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,5 +1,7 @@
 local lsp_zero = require('lsp-zero')
 
+lsp_zero.preset("recommended")
+
 lsp_zero.on_attach(function(client, bufnr)
     local opts = {buffer = bufnr, remap = false}
 
@@ -17,15 +19,22 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = {'pylsp'},
-    handlers = {
-        lsp_zero.default_setup,
-        lua_ls = function()
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            require('lspconfig').lua_ls.setup(lua_opts)
-        end,
-    }
+    ensure_installed = { "lua_ls", "pylsp" },
 })
+require("mason-lspconfig").setup_handlers {
+    -- The first entry (without a key) will be the default handler
+    -- and will be called for each installed server that doesn't have
+    -- a dedicated handler.
+    function (server_name) -- default handler (optional)
+        require("lspconfig")[server_name].setup {}
+    end,
+    -- Next, you can provide a dedicated handler for specific servers.
+    -- For example, a handler override for the `rust_analyzer`:
+    --["rust_analyzer"] = function ()
+    --    require("rust-tools").setup {}
+    --end
+}
+
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
@@ -44,3 +53,4 @@ cmp.setup({
         ['<C-Space>'] = cmp.mapping.complete(),
     }),
 })
+

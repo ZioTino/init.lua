261395129e02841b3249bf1da3f2e65886375b7c
Martino Gabrielli <m.chiodaiolo@gmail.com>
2025-07-17 13:37:55 +0200

    Added telescope fix for invalid window id: 1000

--- lua/tino/lazy/telescope.lua
+++ lua/tino/lazy/telescope.lua
@@ -6,7 +6,22 @@ return {
         { "nvim-telescope/telescope-media-files.nvim" },
     },
     config = function()
+        -- Fix for "Invalid window id: 1000 error"
+        -- https://github.com/nvim-treesitter/nvim-treesitter/issues/7952
+        local actions = require("telescope.actions")
+        local open_after_tree = function(prompt_bufnr)
+            vim.defer_fn(function()
+                actions.select_default(prompt_bufnr)
+            end, 100) -- Delay allows filetype and plugins to settle before opening
+        end
+
         require("telescope").setup({
+            defaults = {
+                mappings = {
+                    i = { ["<CR>"] = open_after_tree },
+                    n = { ["<CR>"] = open_after_tree },
+                },
+            },
             extensions = {
                 media_files = {
                     -- filetypes whitelist
